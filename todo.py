624fdf1b3999cdc97a030d11fd29017796e26add
Tássio Auad <auadtassio@gmail.com>
2021-05-17 22:07:51 -0300

    feat: Atualizar Todos Iniciada a funcionalidade de atualizar ToDos

--- todo.py
+++ todo.py
@@ -13,14 +13,30 @@ class ToDo:
     def id(self):
         return self.__id
 
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
     @property
     def titulo(self):
         return self.__titulo
+
+    @titulo.setter
+    def titulo(self, value):
+        self.__titulo
 
     @property
     def descricao(self):
         return self.__descricao
 
+    @descricao.setter
+    def descricao(self, value):
+        self.__descricao = value
+
     @property
     def data(self):
         return self.__data
+
+    @data.setter
+    def data(self, value):
+        self.__data = value
